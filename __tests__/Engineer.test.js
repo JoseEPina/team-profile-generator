ec71fc6e293358df2d7083654c8f54b90e4d9ee9
JoseEPina <jose.edpina@gmail.com>
2021-09-29 23:50:18 -0600

    Created Test for Engineer Obj: FAILED.

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,15 @@
+const Engineer = require('../lib/Engineer');
+
+test('creates an engineer object', () => {
+   const engineer = new Engineer('Frank', 32, 'frankopoole@space.com', 'frankopoole');
+
+   expect(engineer.name).toBe('Frank');
+   expect(engineer.id).toBe(32);
+   expect(engineer.email).toBe('frankopoole@space.com');
+   expect(engineer.github).toBe(frankopoole);
+   expect(engineer.getName()).toBe('Frank');
+   expect(engineer.getId()).toBe(32);
+   expect(engineer.getEmail()).toBe('frankopoole@space.com');
+   expect(engineer.getGithub()).toBe(frankopoole);
+   expect(engineer.getRole()).toBe('engineer');
+});
