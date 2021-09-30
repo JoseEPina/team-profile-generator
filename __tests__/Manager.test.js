9afe34eb7190b6e446fee31273041776d0b24fc8
JoseEPina <jose.edpina@gmail.com>
2021-09-29 23:17:29 -0600

    Created Obj Manager test case: FAILED.

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,15 @@
+const Manager = require('../lib/Manager');
+
+test('creates an manager object', () => {
+   const manager = new Manager('Dave', 31, 'dave@mail.com', 2001);
+
+   expect(manager.name).toBe('Dave');
+   expect(manager.id).toBe(31);
+   expect(manager.email).toBe('dave@mail.com');
+   expect(manager.officeNumber).toBe(2001);
+   expect(manager.getName()).toBe('Dave');
+   expect(manager.getId()).toBe(31);
+   expect(manager.getEmail()).toBe('dave@mail.com');
+   expect(manager.getOfficeNumber()).toBe(2001);
+   expect(manager.getRole()).toBe('Manager');
+});
